cf517ce1d7a8e89e576729682a4da88c906f39e4
Filippo Tessarotto <zoeslam@gmail.com>
2023-03-15 09:45:28 +0100

    Add support for PHPUnit 10

---
+++ src/DOMAssert.php
@@ -0,0 +1,163 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of PHPUnit DOM Assertions.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPUnit\Framework;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * @author     Sebastian Bergmann <sebastian@phpunit.de>
+ * @author     Jeff Welch <whatthejeff@gmail.com>
+ * @copyright  Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ */
+final class DOMAssert
+{
+    /**
+     * Assert the presence, absence, or count of elements in a document matching
+     * the CSS $selector, regardless of the contents of those elements.
+     *
+     * The first argument, $selector, is the CSS selector used to match
+     * the elements in the $actual document.
+     *
+     * The second argument, $count, can be either boolean or numeric.
+     * When boolean, it asserts for presence of elements matching the selector
+     * (true) or absence of elements (false).
+     * When numeric, it asserts the count of elements.
+     *
+     * assertSelectCount("#binder", true, $xml);  // any?
+     * assertSelectCount(".binder", 3, $xml);     // exactly 3?
+     *
+     * @param array{"<"?: int, ">"?: int, "<="?: int, ">="?: int}|bool|int $count
+     *
+     * @throws Exception
+     */
+    public static function assertSelectCount(
+        string $selector,
+        array|bool|int $count,
+        \DOMDocument|string $actual,
+        string $message = '',
+        bool $isHtml = true
+    ): void {
+        self::assertSelectEquals(
+            $selector,
+            null,
+            $count,
+            $actual,
+            $message,
+            $isHtml
+        );
+    }
+
+    /**
+     * assertSelectRegExp("#binder .name", "/Mike|Derek/", true, $xml); // any?
+     * assertSelectRegExp("#binder .name", "/Mike|Derek/", 3, $xml);    // 3?
+     *
+     * @param array{"<"?: int, ">"?: int, "<="?: int, ">="?: int}|bool|int $count
+     *
+     * @throws Exception
+     */
+    public static function assertSelectRegExp(
+        string $selector,
+        string $pattern,
+        array|bool|int $count,
+        \DOMDocument|string $actual,
+        string $message = '',
+        bool $isHtml = true
+    ): void {
+        self::assertSelectEquals(
+            $selector,
+            "regexp:{$pattern}",
+            $count,
+            $actual,
+            $message,
+            $isHtml
+        );
+    }
+
+    /**
+     * assertSelectEquals("#binder .name", "Chuck", true,  $xml);  // any?
+     * assertSelectEquals("#binder .name", "Chuck", false, $xml);  // none?
+     *
+     * @param array{"<"?: int, ">"?: int, "<="?: int, ">="?: int}|bool|int $count
+     *
+     * @throws Exception
+     */
+    public static function assertSelectEquals(
+        string $selector,
+        ?string $content,
+        array|bool|int $count,
+        \DOMDocument|string $actual,
+        string $message = '',
+        bool $isHtml = true
+    ): void {
+        $crawler = new Crawler();
+
+        if ($actual instanceof \DOMDocument) {
+            $crawler->addDocument($actual);
+        } elseif ($isHtml) {
+            $crawler->addHtmlContent($actual);
+        } else {
+            $crawler->addXmlContent($actual);
+        }
+
+        $crawler = $crawler->filter($selector);
+
+        if (\is_string($content)) {
+            $crawler = $crawler->reduce(static function (Crawler $node) use ($content) {
+                $text = $node->text(null, true);
+
+                if ('' === $content) {
+                    return '' === $text;
+                }
+
+                if (preg_match('/^regexp\s*:\s*(.*)/i', $content, $matches)) {
+                    return (bool) preg_match($matches[1], $text);
+                }
+
+                return str_contains($text, $content);
+            });
+        }
+
+        $found = \count($crawler);
+
+        if (is_numeric($count)) {
+            Assert::assertSame($count, $found, $message);
+        } elseif (\is_bool($count)) {
+            if ($count) {
+                Assert::assertGreaterThan(0, $found, $message);
+            } else {
+                Assert::assertSame(0, $found, $message);
+            }
+        } elseif (\is_array($count)
+            && (isset($count['>']) || isset($count['<'])
+                || isset($count['>=']) || isset($count['<=']))) {
+            if (isset($count['>'])) {
+                Assert::assertGreaterThan($count['>'], $found, $message);
+            }
+
+            if (isset($count['>='])) {
+                Assert::assertGreaterThanOrEqual($count['>='], $found, $message);
+            }
+
+            if (isset($count['<'])) {
+                Assert::assertLessThan($count['<'], $found, $message);
+            }
+
+            if (isset($count['<='])) {
+                Assert::assertLessThanOrEqual($count['<='], $found, $message);
+            }
+        } else {
+            throw new Exception('Invalid count format');
+        }
+    }
+}
---
+++ tests/DOMAssertTest.php
@@ -0,0 +1,359 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of PHPUnit DOM Assertions.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPUnit\Framework\Tests;
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\DOMAssert;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author     Sebastian Bergmann <sebastian@phpunit.de>
+ * @author     Jeff Welch <whatthejeff@gmail.com>
+ * @copyright  Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ *
+ * @internal
+ */
+final class DOMAssertTest extends TestCase
+{
+    private string $html;
+
+    protected function setUp(): void
+    {
+        $html = file_get_contents(
+            __DIR__.'/_files/SelectorAssertionsFixture.html'
+        );
+
+        static::assertNotFalse($html);
+
+        $this->html = $html;
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountPresentTrue(): void
+    {
+        $selector = 'div#test_id';
+        $count = true;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountPresentFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = 'div#non_existent';
+        $count = true;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountNotPresentTrue(): void
+    {
+        $selector = 'div#non_existent';
+        $count = false;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectNotPresentFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = 'div#test_id';
+        $count = false;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountChildTrue(): void
+    {
+        $selector = '#my_ul > li';
+        $count = 3;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountChildFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = '#my_ul > li';
+        $count = 4;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountAdjacentSiblingTrue(): void
+    {
+        $selector = 'div + div + div';
+        $count = 2;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountAdjacentSiblingFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = '#test_children + div';
+        $count = 1;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountDescendantTrue(): void
+    {
+        $selector = '#my_ul li';
+        $count = 3;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountDescendantFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = '#my_ul li';
+        $count = 4;
+
+        DOMAssert::assertSelectCount($selector, $count, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountGreaterThanTrue(): void
+    {
+        $selector = '#my_ul > li';
+        $range = ['>' => 2];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountGreaterThanFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = '#my_ul > li';
+        $range = ['>' => 3];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountGreaterThanEqualToTrue(): void
+    {
+        $selector = '#my_ul > li';
+        $range = ['>=' => 3];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountGreaterThanEqualToFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = '#my_ul > li';
+        $range = ['>=' => 4];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountLessThanTrue(): void
+    {
+        $selector = '#my_ul > li';
+        $range = ['<' => 4];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountLessThanFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = '#my_ul > li';
+        $range = ['<' => 3];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountLessThanEqualToTrue(): void
+    {
+        $selector = '#my_ul > li';
+        $range = ['<=' => 3];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountLessThanEqualToFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = '#my_ul > li';
+        $range = ['<=' => 2];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountRangeTrue(): void
+    {
+        $selector = '#my_ul > li';
+        $range = ['>' => 2, '<' => 4];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectCount
+     */
+    public function testAssertSelectCountRangeFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = '#my_ul > li';
+        $range = ['>' => 1, '<' => 3];
+
+        DOMAssert::assertSelectCount($selector, $range, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectEquals
+     */
+    public function testAssertSelectEqualsContentPresentTrue(): void
+    {
+        $selector = 'span.test_class';
+        $content = 'Test Class Text';
+
+        DOMAssert::assertSelectEquals($selector, $content, true, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectEquals
+     */
+    public function testAssertSelectEqualsContentPresentFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = 'span.test_class';
+        $content = 'Test Nonexistent';
+
+        DOMAssert::assertSelectEquals($selector, $content, true, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectEquals
+     */
+    public function testAssertSelectEqualsContentNotPresentTrue(): void
+    {
+        $selector = 'span.test_class';
+        $content = 'Test Nonexistent';
+
+        DOMAssert::assertSelectEquals($selector, $content, false, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectEquals
+     */
+    public function testAssertSelectEqualsContentNotPresentFalse(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $selector = 'span.test_class';
+        $content = 'Test Class Text';
+
+        DOMAssert::assertSelectEquals($selector, $content, false, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectEquals
+     */
+    public function testDOMDocument(): void
+    {
+        $dom = new \DOMDocument();
+        DOMAssert::assertSelectEquals('', null, false, $dom);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectRegExp
+     */
+    public function testAssertSelectRegExpContentPresentTrue(): void
+    {
+        $selector = 'span.test_class';
+        $regexp = '/Test.*Text/';
+
+        DOMAssert::assertSelectRegExp($selector, $regexp, true, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectRegExp
+     */
+    public function testAssertNormalizedWhitespace(): void
+    {
+        $selector = 'span.test_class';
+        $regexp = '/^Test.*Text$/';
+
+        DOMAssert::assertSelectRegExp($selector, $regexp, true, $this->html);
+    }
+
+    /**
+     * @covers \PHPUnit\Framework\DOMAssert::assertSelectRegExp
+     */
+    public function testAssertSelectRegExpContentPresentFalse(): void
+    {
+        $selector = 'span.test_class';
+        $regexp = '/Nonexistant/';
+
+        DOMAssert::assertSelectRegExp($selector, $regexp, false, $this->html);
+    }
+}
